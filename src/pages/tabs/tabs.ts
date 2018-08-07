ba0a29d45f267bff08f0b516c08e34a2a08f6b3a
Erik <erik.steinman@hotmail.com>
2018-08-07 13:22:55 +0200

    add inject of calculator-page to tabs.ts and link in tabs.html

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 
+import { CalculatorPage } from '../calculator/calculator'
+
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
+  calculatorTab = CalculatorPage;
+  aboutTab = AboutPage
 
-
-  tab2Root = AboutPage;
   constructor() {
 
   }
