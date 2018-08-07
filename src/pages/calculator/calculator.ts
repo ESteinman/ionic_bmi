fe164f00dd380d829dfabaaf1f70e96d5c1bb70c
Erik <erik.steinman@hotmail.com>
2018-08-07 14:02:30 +0200

    app works, but results dont get printed

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 
 
 
@@ -22,7 +22,7 @@ export class CalculatorPage {
     if (this.weight > 0 && this.height > 0) {
       let finalBmi = this.weight / (this.height / 100 * this.height / 100);
       this.bmiValue = parseFloat(finalBmi.toFixed(2));
-      this.setBMImessage();
+      this.setBMIMessage();
     }
   }
 
