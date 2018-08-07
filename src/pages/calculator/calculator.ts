6fdcdc7664af6a30ab897bad9c0b07e5f5dccdec
Erik <erik.steinman@hotmail.com>
2018-08-07 13:35:56 +0200

    [WIP] add this.message to calulator

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -12,10 +12,27 @@ export class CalculatorPage {
   height: number;
   weight: number;
 
+  bmiValue: number;
+  bmiMessage: string;
+
   constructor() {
   }
 
   calculateBMI(){
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = "Underweight"
+    }
 
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "Overweight"
+    }
+
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
   }
 }
